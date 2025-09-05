ebeee083f2159f63011f988674f84a1fea915b05
DevRoberto21 <robertodev2026@gmail.com>
2025-09-05 16:39:27 -0300

    atualização do código

--- semana1/teste1.py
+++ semana1/teste1.py
@@ -1,49 +1,73 @@
-def soma(num1,num2):
-    return num1 + num2
+def soma(n1,n2):
+    return round(n1 + n2,2)
 
-def subtracao(num1,num2):
-    return num1 - num2
+def subtracao(n1,n2):
+    return round(n1 - n2,2)
 
-def div(num1,num2):
-    return num1/num2
+def div(n1,n2):
+    return round(n1/n2,2)
 
-def mult(num1,num2):
-    return num1 * num2
-
-
+def mult(n1,n2):
+    return round(n1 * n2,2)
 
 
 
-while(True):
-    print("Calculadora\n Digite 1 para somar \n Digite 2 para subtrair \n Digite 3 para dividir \n Digite 4 para multiplicar \n Digite 0 para saír")
-    opcao = float(input("Comando:"))
-    if (opcao == 0):
-        print("Encerrando")
-        break
-    
-    elif ( 1 <= opcao <= 4 ):
-        num1 = float(input("Número 1:"))
-        num2 = float(input("Número 2:"))
+historico = []
+try:
+    while(True):
+        print("Calculadora\n Digite 1 para somar \n Digite 2 para subtrair \n Digite 3 para dividir \n Digite 4 para multiplicar \n Digite 0 para saír")
+        opcao = float(input("Comando:"))
+        
+        if (opcao == 0):
+            print("Encerrando")
+            break
+        
+        elif ( 1 <= opcao <= 4 ):
+            num1 = float(input("Número 1:"))
+            num2 = float(input("Número 2:"))
 
-        if (opcao == 1):
+            if (opcao == 1):
+                print("---"*10)
+                resultado = soma(num1,num2)
+                print(f"resultado:{resultado}")
+                historico.append(resultado)
+                print("---"*10)
+
+            elif(opcao ==2):
+                print("---"*10)
+                resultado = subtracao(num1,num2)
+                print(f"resultado:{resultado}")
+                historico.append(resultado)
+                print("---"*10)
+
+            elif(opcao == 3):
+                print("---"*10)
+                resultado = div(num1,num2)
+                print(f"resultado:resultado,2)")
+                historico.append(resultado)
+                print("---"*10)
+
+            elif(opcao == 4):
+                print("---"*10)
+                resultado = mult(num1,num2)
+                print(f"resultado:{resultado}")
+                historico.append(resultado)
+                print("---"*10)
+                
+        else:
             print("---"*10)
-            print(f"resultado:{soma(num1,num2)}")
+            print("Comando inválido, tente novamente!")
             print("---"*10)
 
-        elif(opcao ==2):
-            print("---"*10)
-            print(f"resultado:{subtracao(num1,num2)}")
-            print("---"*10)
+    print("---"*10)
+    print("Histórico de resultados:")
+    for i in historico:
+        print(i)
+    print("---"*10)
 
-        elif(opcao == 3):
-            print("---"*10)
-            print(f"resultado:{round(div(num1,num2),2)}")
-            print("---"*10)
 
-        elif(opcao == 4):
-            print("---"*10)
-            print(f"resultado:{mult(num1,num2)}")
-            print("---"*10)
-    else:
-        print("Comando inválido, tente novamente!")
 
+except ValueError:
+    print("Somente números.")
+except ZeroDivisionError:
+    print("Não é possivel dividir por 0")
