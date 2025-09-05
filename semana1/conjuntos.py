f9de3c92d6b54cf47201d982e4223ef9cbce75b6
DevRoberto21 <robertodev2026@gmail.com>
2025-09-05 16:39:53 -0300

    conteudos extras

---
+++ semana1/conjuntos.py
@@ -0,0 +1,4 @@
+conjuntoA = {0,2,4,6}
+conjuntoB = {1,3,5,7}
+
+print(conjuntoA ^ conjuntoB)
---
+++ semana1/dicionarios.py
@@ -0,0 +1,14 @@
+pessoa1 = {
+    "nome" : "Roberto",
+    "idade" : 20
+}
+
+pessoa2 = {
+    "nome" : "Jean",
+    "idade" : 22
+}
+
+lista = [pessoa1,pessoa2]
+
+for pessoa in lista:
+    print(pessoa["idade"])
